5e36f85e6fe64b8e2e221979a2ef6b83732c8e1e
szymon-solak <yoshuro@hotmail.com>
2021-10-17 15:43:07 +0200

    improvement(patterns): add common patterns

--- src/cellular_automata/core.clj
+++ src/cellular_automata/core.clj
@@ -2,7 +2,8 @@
   (:gen-class)
   (:require
    [cellular-automata.rules :as rules]
-   [cellular-automata.renderers :as renderers]))
+   [cellular-automata.renderers :as renderers]
+   [cellular-automata.patterns :as patterns]))
 
 (defn process-turn [grid rule]
   (vec
@@ -17,32 +18,15 @@
 (defn run-automata [grid rule render]
   (loop [next-grid grid]
     (render next-grid)
-    (Thread/sleep 50)
+    (Thread/sleep 200)
     (recur (process-turn next-grid rule))))
-
-(def gosper-glider-gun [[0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0]
-                        [0 1 1 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 1 1 0 0 0 0 0 0 0 0 1 0 0 0 1 0 1 1 0 0 0 0 1 0 1 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 0 0 1 0 0 0 0 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 1 0 0 0 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]
-                        [0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0]])
 
 (defn -main
   [& args]
 
-  ;; Show initial pattern, wait for any input to start
-  (let [window (renderers/gui-create-window 1200 768)]
-    (renderers/gui-draw window gosper-glider-gun)
-    (read-line))
-
   ;; Run the pattern in a gui
   (let [window (renderers/gui-create-window 1200 768)]
-    ((run-automata gosper-glider-gun rules/conway (partial renderers/gui-draw window))
+    ((run-automata patterns/glider rules/conway (partial renderers/gui-draw window))
      (renderers/gui-destroy-window window)))
 
   ;; Run the pattern in a terminal
